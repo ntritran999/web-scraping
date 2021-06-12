6019bd0a19e67cab7b962e8f1821482b7924026e
ntritran999 <81624097+ntritran999@users.noreply.github.com>
2021-06-12 12:56:57 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,30 @@
+from selenium.webdriver import Chrome 
+from selenium.webdriver.chrome.options import Options
+from bs4 import BeautifulSoup
+import time
+
+
+
+opts = Options()
+opts.add_argument('--ignore-certificate-errors')
+opts.add_argument('--ignore-ssl-errors')
+opts.headless = True
+browser = Chrome('./chromedriver.exe', options=opts)
+browser.get('https://www.frontendmentor.io/challenges')
+
+source = browser.page_source
+soup = BeautifulSoup(source, 'html.parser')
+
+challengecard_contents = soup.find_all('div', class_='ChallengeCard__Content-luiznt-4 kwXWoS')
+newbies_links = dict()
+
+for challenge in challengecard_contents:
+    name = challenge.find('h3').find('a')
+    level = challenge.find('span').text
+    if level == '1':
+        newbies_links[name.text] = 'https://www.frontendmentor.io' + name['href']
+
+time.sleep(5)
+browser.quit()
+for title, site in newbies_links.items():
+    print(title + ': ' + site, '\n')
